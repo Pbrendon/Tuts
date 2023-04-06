af4cddb7dee705bc253fcc2ca930fa34264572db
pbrendon <brandonpetit23@yahoo.com>
2023-04-05 22:56:03 -0400

    first commit trying to understand this

---
+++ dojo-blog/src/Navbar.js
@@ -0,0 +1,23 @@
+const Navbar = () => {
+    return (
+     <nav className="navbar>">
+     <h1>The Dojo Blog</h1>
+     <div classname="links">
+    <a href="/">Home</a>
+         <a href="/create">style={{
+      color: "white",
+      backgroundColor: '#f1356d',
+      borderRadius: '8px'
+         }} New Blog</a>
+     </div>
+     </nav>
+    );
+    }
+
+    export default Navbar;
+
+
+
+
+
+
